f0aa287070c412e54d60c6c4d4e37596214520cf
balasrinivasanammu <balajiit88@gmail.com>
2025-02-05 15:05:18 +0530

    Add files via upload

--- strip1.py
+++ strip1.py
@@ -1,14 +1,27 @@
 txt = "---chennai----bbcckk????"
 
-x = txt.strip("")
+x = txt.rstrip("-bk?")
 
 print(x)
+
+
+
+
 '''
+
+
+
+
+
+
+
+
+
 The strip() method in Python is used to remove leading and trailing
 whitespace characters (such as spaces, tabs, and newline characters)
 from a string. It can also be used to remove specific characters
 from the start and end of a string if you specify them
-'''
+
 
 # Example 2: Using strip() to remove specific characters
 text = "xxchennai, cityxx"
@@ -27,4 +40,6 @@ cleaned_input = user_input.strip()
 
 print(f"Original input: '{user_input}'")
 print(f"Cleaned input: '{cleaned_input}'")
+
+'''
 
